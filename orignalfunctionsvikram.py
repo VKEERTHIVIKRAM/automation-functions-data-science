4b63cb001cafa3fa3cd3f01d30a5ce4dbcf7aa89
VKEERTHIVIKRAM <61373970+VKEERTHIVIKRAM@users.noreply.github.com>
2020-02-23 13:48:40 +0530

    Update orignalfunctionsvikram.py

--- orignalfunctionsvikram.py
+++ orignalfunctionsvikram.py
@@ -166,63 +166,6 @@ def df_summary_specific(df,target=None,missing_only='no',impossible_only='no',ou
         raise ValueError('Can only input yes or no')
     return d            
 
-
-def svisualizer(x, ncluster):
-    import matplotlib.pyplot as plt
-    from sklearn.cluster import KMeans
-    import numpy as np
-    from matplotlib import cm
-    from sklearn.metrics import silhouette_samples 
-
-    km = KMeans(n_clusters=ncluster, init='k-means++', n_init=10, max_iter=300, tol=1e-04, random_state=0)
-    y_km = km.fit_predict(x)
-
-    cluster_labels = np.unique(y_km)
-    n_clusters = cluster_labels.shape[0]
-    silhouette_vals = silhouette_samples(x, y_km, metric='euclidean')
-    y_ax_lower, y_ax_upper = 0, 0
-
-    yticks = []
-    for i, c in enumerate(cluster_labels):
-        c_silhouette_vals = silhouette_vals[y_km==c]
-        c_silhouette_vals.sort()
-        y_ax_upper += len(c_silhouette_vals)
-        color = cm.jet(i / n_clusters)
-        plt.barh(range(y_ax_lower, y_ax_upper), c_silhouette_vals, height=1.0, edgecolor='none', color=color)
-
-        yticks.append((y_ax_lower + y_ax_upper) / 2)
-        y_ax_lower += len(c_silhouette_vals)
-
-    silhouette_avg = np.mean(silhouette_vals)
-    plt.axvline(silhouette_avg, color="red", linestyle="--") 
-
-    plt.yticks(yticks, cluster_labels + 1)
-    plt.ylabel('Cluster')
-    plt.xlabel('Silhouette coefficient')
-
-    plt.tight_layout()
-    plt.show()	
-
-
-def cluster_plot(data, nclusters):
-    from sklearn.cluster import KMeans
-    import matplotlib.pyplot as plt
-    X = data.copy()
-
-    km = KMeans(n_clusters=nclusters, init='random', n_init=10, max_iter=300, tol=1e-04, random_state=0)
-    y_km = km.fit_predict(X)
-
-
-    # Visualize it:
-    plt.figure(figsize=(8, 6))
-    plt.scatter(X[:,0], X[:,1], c=km.labels_.astype(float))
-
-    # plot the centroids
-    plt.scatter(km.cluster_centers_[:, 0], km.cluster_centers_[:, 1], s=250, marker='*', c='red', label='centroids')
-    plt.legend(scatterpoints=1)
-    plt.grid()
-    plt.show()
-
 def corr_matrix(df,annot=True):
     """Paramters-
        df : Dataframe
@@ -254,9 +197,9 @@ def corr_matrix(df,annot=True):
     sns.heatmap(df, mask=mask, cmap=cmap, vmax=.3, center=0, annot=annot, square=True, linewidths=.5, cbar_kws={"shrink": .5})
 
 
-def model_eval(algo,Xtrain,ytrain,Xtest,ytest,voting=None):
+def model_eval_classifier(algo,Xtrain,ytrain,Xtest,ytest,voting=None):
     """Used for evaluating model performance for classification
-       Can also be used when algorithim is a VOting Classifier
+       Can also be used when algorithim is a Voting Classifier
        Parameters-
        algo : Model Algorithim used. eg random forest,logistic regression etc.
        Xtrain : Train data without target variable.
@@ -331,8 +274,6 @@ def features_to_drop(df,corr_value=0.95):
     
     """
     Used to drop Highly correlated columns.
-    To remove correlated features, used this gem of a code from here:
-    https://chrisalbon.com/machine_learning/feature_selection/drop_highly_correlated_features
     Parameters-
     df : dataFrame
     corr_value : Correlation Value .Default is 0.95
