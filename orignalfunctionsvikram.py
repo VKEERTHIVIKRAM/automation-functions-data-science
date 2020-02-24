3c5fca8f4a2754d94fb6ea4f372d580dff09caa2
V KEERTHI VIKRAM <61373970+VKEERTHIVIKRAM@users.noreply.github.com>
2020-02-24 11:36:27 +0530

    Add files via upload

--- orignalfunctionsvikram.py
+++ orignalfunctionsvikram.py
@@ -117,7 +117,7 @@ def  df_summary(df,target=None,num_but_cat_list=[]):
     return summary
                                         
 
-def df_summary_specific(df,target=None,missing_only='no',impossible_only='no',outliers_only='no'):
+def df_summary_specific(df,target=None,num_but_cat_list=[],missing_only='no',impossible_only='no',outliers_only='no'):
     """ Parameters-        
         only_missing : 'yes' or 'no'. Default is 'no'
         only_impossible : 'yes' or 'no'. Default is 'no'
@@ -145,7 +145,7 @@ def df_summary_specific(df,target=None,missing_only='no',impossible_only='no',ou
         dfn=df
     else:
         raise ValueError('Target column inputed not in DataFrame')
-    a=df_summary(dfn)
+    a=df_summary(dfn,num_but_cat_list=num_but_cat_list)
     if missing_only=='yes':
         b=a[a['Missing']!=0]
     elif missing_only=='no':
@@ -165,6 +165,63 @@ def df_summary_specific(df,target=None,missing_only='no',impossible_only='no',ou
     else:
         raise ValueError('Can only input yes or no')
     return d            
+
+
+def svisualizer(x, ncluster):
+    import matplotlib.pyplot as plt
+    from sklearn.cluster import KMeans
+    import numpy as np
+    from matplotlib import cm
+    from sklearn.metrics import silhouette_samples 
+
+    km = KMeans(n_clusters=ncluster, init='k-means++', n_init=10, max_iter=300, tol=1e-04, random_state=0)
+    y_km = km.fit_predict(x)
+
+    cluster_labels = np.unique(y_km)
+    n_clusters = cluster_labels.shape[0]
+    silhouette_vals = silhouette_samples(x, y_km, metric='euclidean')
+    y_ax_lower, y_ax_upper = 0, 0
+
+    yticks = []
+    for i, c in enumerate(cluster_labels):
+        c_silhouette_vals = silhouette_vals[y_km==c]
+        c_silhouette_vals.sort()
+        y_ax_upper += len(c_silhouette_vals)
+        color = cm.jet(i / n_clusters)
+        plt.barh(range(y_ax_lower, y_ax_upper), c_silhouette_vals, height=1.0, edgecolor='none', color=color)
+
+        yticks.append((y_ax_lower + y_ax_upper) / 2)
+        y_ax_lower += len(c_silhouette_vals)
+
+    silhouette_avg = np.mean(silhouette_vals)
+    plt.axvline(silhouette_avg, color="red", linestyle="--") 
+
+    plt.yticks(yticks, cluster_labels + 1)
+    plt.ylabel('Cluster')
+    plt.xlabel('Silhouette coefficient')
+
+    plt.tight_layout()
+    plt.show()	
+
+
+def cluster_plot(data, nclusters):
+    from sklearn.cluster import KMeans
+    import matplotlib.pyplot as plt
+    X = data.copy()
+
+    km = KMeans(n_clusters=nclusters, init='random', n_init=10, max_iter=300, tol=1e-04, random_state=0)
+    y_km = km.fit_predict(X)
+
+
+    # Visualize it:
+    plt.figure(figsize=(8, 6))
+    plt.scatter(X[:,0], X[:,1], c=km.labels_.astype(float))
+
+    # plot the centroids
+    plt.scatter(km.cluster_centers_[:, 0], km.cluster_centers_[:, 1], s=250, marker='*', c='red', label='centroids')
+    plt.legend(scatterpoints=1)
+    plt.grid()
+    plt.show()
 
 def corr_matrix(df,annot=True):
     """Paramters-
